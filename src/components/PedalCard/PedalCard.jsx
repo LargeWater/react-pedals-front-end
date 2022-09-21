b99cae21fc59cbe399b7f47373b9f9a7753358bd
Nicholas Bailey <nickcyg1992@gmail.com>
2022-09-21 16:49:10 -0500

    header background and card color

--- src/components/PedalCard/PedalCard.jsx
+++ src/components/PedalCard/PedalCard.jsx
@@ -8,18 +8,18 @@ function PedalCard({pedal, handleDeletePedal, user}) {
         src={pedal.photo}
         alt="pedal"
         className="card-img-top"
-        style={{width: "auto", height: "auto", objectFit: "contain", maxHeight: "400px", backgroundColor: "#F7F0F5", mixBlendMode: "multiply"}}
+        style={{width: "auto", height: "auto", objectFit: "contain", maxHeight: "400px"}}
       />
-      <div className="card-body" style={{backgroundColor: "#F7F0F5", mixBlendMode: "multiply"}}>
+      <div className="card-body">
         <h2 className="card-text">{pedal.name}</h2>
         <h5 className="card-text">{pedal.type}</h5>
         <p className="card-text">{pedal.description}</p>
       </div>
       {user?.profile === pedal.owner._id &&
-        <div className="card-footer" style={{backgroundColor: "#F7F0F5"}}>
+        <div className="card-footer">
           <Link
             className='btn btn-sm'
-            style={{backgroundColor:"#8D6A9F", color: "#f7f0f5"}}
+            style={{backgroundColor:"#5a4366", color: "#f7f0f5"}}
             to='/edit'
             state={{pedal}}
           >
@@ -27,7 +27,7 @@ function PedalCard({pedal, handleDeletePedal, user}) {
           </Link>
           <button 
             className="btn btn-sm m-left"
-            style={{backgroundColor:"#8D6A9F", color: "#f7f0f5"}}
+            style={{backgroundColor:"#5a4366", color: "#f7f0f5"}}
             onClick={() => handleDeletePedal(pedal._id)}
           >
             Delete
