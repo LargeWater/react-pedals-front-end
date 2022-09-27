1a33f4ea6a0652b42220cd4744eec3767ace9702
Nicholas Bailey <nickcyg1992@gmail.com>
2022-09-27 11:56:59 -0500

    change navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,67 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
-const NavBar = ({ user, handleLogout }) => {
+// const NavBar = ({ user, handleLogout }) => {
+//   return (
+//     <>
+//       <header className="App-header" style={{overflow: 'hidden'}}>
+//         React Pedals
+//         {user ?
+//           <nav>
+//             <Link to="/">Pedals</Link>
+//             {/* <Link to="/add">Add Pedal</Link> */}
+//             <Link to='/profiles'>Profiles</Link>
+//             <Link to='/pedalboards'>Pedalboards</Link>
+//             <Link to="" onClick={handleLogout}>Log Out</Link>
+//             <Link to="/changePassword">Change Password</Link>
+//           </nav>
+//         :
+//           <nav>
+//             <Link to="/login">Log In</Link>
+//             <Link to="/signup">Sign Up</Link>
+//           </nav>
+//         }
+//       </header>
+//     </>
+//   )
+// }
+
+// export default NavBar
+
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const BasicExample = ({ user, handleLogout }) => {
   return (
     <>
-      <header className="App-header" style={{overflow: 'hidden'}}>
-        React Pedals
-        {user ?
-          <nav>
-            <Link to="/">Pedals</Link>
-            {/* <Link to="/add">Add Pedal</Link> */}
-            <Link to='/profiles'>Profiles</Link>
-            <Link to='/pedalboards'>Pedalboards</Link>
-            <Link to="" onClick={handleLogout}>Log Out</Link>
-            <Link to="/changePassword">Change Password</Link>
-          </nav>
-        :
-          <nav>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
-          </nav>
-        }
-      </header>
+      <Navbar collapseOnSelect expand="lg" variant='dark'>
+        <Container>
+          <Navbar.Brand href="#home">React Pedals</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {user ?
+            <Nav className="me-auto">
+              <Nav.Link href="/">Pedals</Nav.Link>
+              <Nav.Link href="/profiles">Profiles</Nav.Link>
+              <Nav.Link href="/pedalboards">Pedalboards</Nav.Link>
+              <NavDropdown title="Account" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/changePassword">Change Password</NavDropdown.Item>
+                <NavDropdown.Item href="" onClick={handleLogout}>Log Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            :
+            <Nav className="me-auto">
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/signup">Signup</Nav.Link>
+            </Nav>
+            }
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  )
+  );
 }
 
-export default NavBar
-
+export default BasicExample;
