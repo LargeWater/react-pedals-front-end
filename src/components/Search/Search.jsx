a3ba369f9fe037ae82def1634f10daab09313ddd
Nicholas Bailey <nickcyg1992@gmail.com>
2022-10-17 15:52:47 -0500

    clean up search page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,38 +1,34 @@
 import { useState } from 'react';
 import SearchList from '../../pages/SearchList/SearchList';
-import Scroll from '../Scroll/Scroll';
+import styles from './Search.module.css';
 
 function Search({pedals}){
   const [searchField, setSearchField] = useState("");
-  const [searchShow, setSearchShow] = useState(false);
 
   const filteredPedals = pedals.filter(pedal => pedal.name.toLowerCase().includes(searchField.toLowerCase()));
 
   const handleChange = e => {
     setSearchField(e.target.value);
-    if(e.target.value===""){
-      setSearchShow(false);
-    }
-    else {
-      setSearchShow(true);
-    }
   };
 
   function searchList() {
-    if (searchShow) {
       return (
-        <Scroll>
+        <>
           <SearchList filteredPedals={filteredPedals} />
-        </Scroll>
+        </>
       );
-    }
   }
 
   return (
-    <div>
+    <>
+    <h1>Search for a pedal</h1>
+    <div className={styles.searchBar}>
       <input type="search" placeholder="Search" value={searchField} onChange={handleChange} />
+    </div>
+    <div className={styles.container}>
       {searchList()}
     </div>
+    </>
   )
 }
 
